208e7c7484ebadd45382059f8c176f5a74bf6308
hamade <hamande@gmail.com>
2022-05-14 17:31:32 +0200

    dssd*

--- resources/views/include/navbar2.blade.php
+++ resources/views/include/navbar2.blade.php
@@ -20,6 +20,21 @@
           </ul>
         </div>
       </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+            <i class="ti-clipboard menu-icon"></i>
+            <span class="menu-title">Création</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="form-elements">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"><a class="nav-link" href="{{URL::to('/ajoutercategorie')}}">Ajouter categorie</a></li>
+              <li class="nav-item"><a class="nav-link" href="{{URL::to('/ajouterproduit')}}">Ajouter Prodiut</a></li>
+              <li class="nav-item"><a class="nav-link" href="{{URL::to('/ajouterslider')}}">Ajout Slider</a></li>
+            </ul>
+          </div>
+        </li>
+        
       <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
           <i class="ti-layout menu-icon"></i>
